97e9ac5e4d590fdf3c4925cd18cc4b5b97ab2851
vishusharmaX <sharmavishwajeet04@gmail.com>
2023-01-10 01:02:52 +0530

    intial

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './styles/App.scss'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './components/Home'
+import Header from './components/Header'
+import {Toaster} from 'react-hot-toast'
+import Cart from './components/Cart'
+
+const App = () => {
+  return (
+   <Router>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/cart' element={<Cart/>}/>
+    </Routes>
+    <Toaster/>
+   </Router>
+  )
+}
+
+export default App
